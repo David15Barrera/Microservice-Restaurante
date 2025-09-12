11ae3e58dfab0561e8297137d783115d900ed40b
David15Barrera <david1315barrera@gmail.com>
2025-09-12 05:39:59 -0600

    fix: Errores

--- restaurantService/src/main/java/com/service/restaurantService/order/infraestructure/outputadapter/feign/CustomerFeign/CustomerFeignClient.java
+++ restaurantService/src/main/java/com/service/restaurantService/order/infraestructure/outputadapter/feign/CustomerFeign/CustomerFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customerClient", url = "http://localhost:8081/api/auth-identity", fallbackFactory = CustomerFeignClientFallbackFactory.class)
+@FeignClient(name = "customerClient", url = "https://microservice-authemploye.onrender.com/api/auth-identity", fallbackFactory = CustomerFeignClientFallbackFactory.class)
 public interface CustomerFeignClient {
 
     @GetMapping("/api/v1/customers/{id}")
