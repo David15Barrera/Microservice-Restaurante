dd2802952f26d44f88933c6b056e5eb39d6b9572
David15Barrera <david1315barrera@gmail.com>
2025-09-12 03:52:51 -0600

    fix: Errores

--- restaurantService/src/main/java/com/service/restaurantService/orderdetail/infrastructure/outputadapter/feign/PromotionFeign/PromotionFeignClient.java
+++ restaurantService/src/main/java/com/service/restaurantService/orderdetail/infrastructure/outputadapter/feign/PromotionFeign/PromotionFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient(name = "promotionClient", url = "http://localhost:8086/api/promotion", fallbackFactory = PromotionFeignClientFallbackFactory.class)
+@FeignClient(name = "promotionClient", url = "https://microservice-promotions-12sn.onrender.com/api/promotion", fallbackFactory = PromotionFeignClientFallbackFactory.class)
 public interface PromotionFeignClient {
     @GetMapping("/api/v1/promotions/{id}")
     PromotionResponse getPromotionById(@PathVariable("id") UUID id);
